324f96c2cc670f8ab0534f343d4fc6859b332095
jamelahallal <jamela.hallal45@gmail.com>
2026-04-19 00:48:01 +0300

    Deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, createContext, useContext, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
